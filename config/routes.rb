72012666cf3ee0c6498357d9df8620f2a9fb19cb
alimangat <ali.raza.mangat5@gmail.com>
2023-08-31 22:36:06 +0500

    Home & About page created, linked and navbar added

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'main#home'
+  get '/about', to: 'main#about'
+
+
+
+
+
+
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
